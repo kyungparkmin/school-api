1713b642da45f27676dc69f3acedad6a8b9e420f
kyungparkmin <kyungparkmin@naver.com>
2022-12-21 15:34:46 +0900

    dd
    d

--- routes/index.js
+++ routes/index.js
@@ -12,23 +12,42 @@ const getSchool = async (name) => {
   return await response.json();
 }
 
-const getMeal = async () => {
+const getMeal = async (sc_code, schul_code) => {
+  const url = `https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=${process.env.NEIS_KEY}&Type=json&plndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${sc_code}&SD_SCHUL_CODE=${schul_code}&MLSV_YMD=20221221`;
 
+  let response = await fetch(url);
+
+  return await response.json();
 }
 
 router.get('/:name', async (req, res, next) => {
-
-
   try {
     const data = await getSchool(req.params.name);
 
-    console.log(data.schoolInfo[1].row[0].SCHUL_NM);
+    const sc_code = data.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE; // 시도교육청코드
+    const schul_code = data.schoolInfo[1].row[0].SD_SCHUL_CODE; // 표준학교코드
 
-    res.status(200).json(data);
+    const meal = await getMeal(sc_code, schul_code);
+
+    const arr = [];
+
+    const breakfast = meal.mealServiceDietInfo[1].row[0].DDISH_NM.split("<br/>");
+    const lunch = meal.mealServiceDietInfo[1].row[1].DDISH_NM.split("<br/>");
+    const dinner = meal.mealServiceDietInfo[1].row[2].DDISH_NM.split("<br/>");
+
+    arr.push({
+      "breakfast" : breakfast,
+      "lunch": lunch,
+      "dinner": dinner
+    });
+
+    res.status(200).json({ menu: arr, data });
   } catch (error) {
     console.error(error);
     next(error);
   }
 })
+
+router.get('/')
 
 module.exports = router;
